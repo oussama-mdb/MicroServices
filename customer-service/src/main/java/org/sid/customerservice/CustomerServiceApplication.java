002c0639b4f94f9466c6e08e67a13b7e18f10384
oussama mouaddab <62763199+oussama-mdb@users.noreply.github.com>
2020-12-31 15:43:03 +0100

    add Customers

---
+++ customer-service/src/main/java/org/sid/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,32 @@
+package org.sid.customerservice;
+
+import org.sid.customerservice.entities.Customer;
+import org.sid.customerservice.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+    @Bean
+    CommandLineRunner start(CustomerRepository customerRepository,
+                            RepositoryRestConfiguration restConfiguration){
+        restConfiguration.exposeIdsFor(Customer.class);
+        return args -> {
+            customerRepository.save(new Customer(null, "oussama", "oussamaMouddab@gmail.com"));
+            customerRepository.save(new Customer(null, "hamza", "Hamza@gmail.com"));
+            customerRepository.save(new Customer(null, "salma", "salma@gmail.com"));
+            customerRepository.findAll().forEach(c->{
+                System.out.println(c.toString());
+            });
+        };
+    }
+
+}
